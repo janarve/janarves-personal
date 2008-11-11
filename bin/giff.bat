ad8a94afb6b23de184470adfd312b8d919f47009
Jan-Arve Saether <jan.arve.saether@trolltech.com>
2008-11-11 22:05:21 +0100

    Fixes:   Some small useful script I use daily.

---
+++ bin/giff.bat
@@ -0,0 +1,4 @@
+SETLOCAL
+SET GIT_EXTERNAL_DIFF=c:\Program Files (x86)\WinMerge\WinMergeU.exe
+git diff
+
---
+++ bin/npp.bat
@@ -0,0 +1,2 @@
+@REM workaround for notepad++, since it has problems opening several relative files
+@start C:\Progra~2\notepad++\notepad++.exe %~f1 %~f2 %~f3 %~f4 %~f5 %~f6
