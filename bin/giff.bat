0b16e8f7f0ba2245cb090a7be1c3cbb6c9db0b67
Jan-Arve Sæther <jan-arve.saether@nokia.com>
2010-11-23 15:38:41 +0100

    New feature: giff file1.txt file2.txt will just show the diff of the two

--- bin/giff.bat
+++ bin/giff.bat
@@ -1,5 +1,10 @@
 @echo off
 REM assumes taht git_external_diff.cmd is in the same path as giff.bat
 SETLOCAL
+IF EXIST "%1" (
+    IF EXIST "%2" (
+        C:\Progra~2\WinMerge\WinMergeU.exe %1 %2
+    )
+)    
 SET GIT_EXTERNAL_DIFF=git_external_diff.cmd
 git diff --exit-code %*
