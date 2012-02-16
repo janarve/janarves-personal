3e3775f6678f14e5b082f9635487ae0968d7c367
Jan-Arve Saether <jan-arve.saether@nokia.com>
2012-02-16 12:01:54 +0100

    Make manipulation of $env:Path more robust.

--- bin/ps1/setcompiler.ps1
+++ bin/ps1/setcompiler.ps1
@@ -2,66 +2,84 @@
 
 # Set the Qt environment; QTDIR QTDEPOT and PATH
 function setCompiler($cpu){
+    $NewINCLUDE  = "t:\3rdparty\openssl64\include"
+    $NewINCLUDE += ";t:\3rdparty\expat\Source\lib"
     
-    if ($env:SETCOMPILER_ORIGINALPATH) {
-        if ($cpu -eq "x86" -or $cpu -eq "amd64") {
-            $env:PATH=$env:SETCOMPILER_ORIGINALPATH
-            $env:Include=$env:SETCOMPILER_ORIGINALINCLUDE
-            $env:Lib=$env:SETCOMPILER_ORIGINALLIB
-        }
-    } else {
-        $env:SETCOMPILER_ORIGINALPATH=$env:PATH
-        $env:SETCOMPILER_ORIGINALINCLUDE=$env:Include
-        $env:SETCOMPILER_ORIGINALLIB=$env:Lib
-    }
-
     if ($cpu -eq "x86") {
-        $env:Lib+="C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\ATLMFC\LIB"
-        $env:Lib+=";C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\LIB"
-        $env:Lib+=";C:\Program Files\Microsoft SDKs\Windows\v7.0\lib"
+        $NewLIB  = "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\ATLMFC\LIB"
+        $NewLIB +=";C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\LIB"
+        $NewLIB +=";C:\Program Files\Microsoft SDKs\Windows\v7.0\lib"
     
-        $env:Include="C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\ATLMFC\INCLUDE;"    
-        
-        $env:PATH+=";C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\BIN"
-        $env:PATH+=";C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\Tools"
-        $env:PATH+=";C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\Tools\bin"
-        $env:PATH+=";C:\Windows\Microsoft.NET\Framework\"
-        $env:PATH+=";C:\Windows\Microsoft.NET\Framework\Microsoft .NET Framework 3.5 (Pre-Release Version)"
-        $env:PATH+=";C:\Windows\Microsoft.NET\Framework\v2.0.50727"    
+        $NewINCLUDE+=";C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\ATLMFC\INCLUDE"
+
+        $NewPATH  = "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\BIN"
+        $NewPATH +=";C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\BIN"
+        $NewPATH +=";C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\Tools"
+        $NewPATH +=";C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\Tools\bin"
+        $NewPATH +=";C:\Windows\Microsoft.NET\Framework"
+        $NewPATH +=";C:\Windows\Microsoft.NET\Framework\Microsoft .NET Framework 3.5 (Pre-Release Version)"
+        $NewPATH +=";C:\Windows\Microsoft.NET\Framework\v2.0.50727"
         
     }
     if ($cpu -eq "amd64") {
-        $env:Lib+=";C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\Lib\amd64"
-        $env:Lib+=";C:\Program Files\Microsoft SDKs\Windows\v7.0\Lib\X64"
-        $env:Lib+=";t:\3rdparty\openssl64\lib"
+        $NewLIB  = "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\Lib\amd64"
+        $NewLIB += ";C:\Program Files\Microsoft SDKs\Windows\v7.0\Lib\X64"
+        $NewLIB += ";t:\3rdparty\openssl64\lib"
     
-        $env:PATH+=";C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\Bin\amd64"
-        $env:PATH+=";C:\Program Files\Microsoft SDKs\Windows\v7.0\Bin\x64"
+        $NewPATH  = "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\Bin\amd64"
+        $NewPATH += ";C:\Program Files\Microsoft SDKs\Windows\v7.0\Bin\x64"
 
     }
 
-    ### common stuff ###
-    $env:Lib+=";t:\3rdparty\expat\bin"
-    $env:Lib+=";t:\dev\devtools\database\lib\msvc"
-    
-    $env:Include+="C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\INCLUDE"
-    $env:Include+=";C:\Program Files\Microsoft SDKs\Windows\v7.0\Include"
-    $env:Include+=";C:\Program Files\Microsoft SDKs\Windows\v7.0\Include\gl" 
 
-    $env:Include+=";t:\dev\devtools\database\include\tds;t:\dev\devtools\database\include\db2;t:\dev\devtools\database\include\fbird"
-    $env:Include+=";t:\dev\devtools\database\include\oci;t:\dev\devtools\database\include\mysql;t:\dev\devtools\database\include\psql"
-    $env:Include+=";t:\3rdparty\openssl64\include"
-    $env:Include+=";t:\3rdparty\expat\Source\lib"
+    ### common stuff ###
+    $NewLIB+=";t:\3rdparty\expat\bin"
+    $NewLIB+=";t:\dev\devtools\database\lib\msvc"
+
+    $NewINCLUDE+=";C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\INCLUDE"
+    $NewINCLUDE+=";C:\Program Files\Microsoft SDKs\Windows\v7.0\Include"
+    $NewINCLUDE+=";C:\Program Files\Microsoft SDKs\Windows\v7.0\Include\gl"
+
+    $NewINCLUDE+=";t:\dev\devtools\database\include\tds;t:\dev\devtools\database\include\db2;t:\dev\devtools\database\include\fbird"
+    $NewINCLUDE+=";t:\dev\devtools\database\include\oci;t:\dev\devtools\database\include\mysql;t:\dev\devtools\database\include\psql"
+    $NewINCLUDE+=";t:\3rdparty\openssl64\include"
+    $NewINCLUDE+=";t:\3rdparty\expat\Source\lib"
     
-    $env:PATH+=";C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE"
-    $env:PATH+=";C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages"
-    $env:PATH+=";C:\Windows\Microsoft.NET\Framework64\v3.5;"
-    $env:PATH+=";C:\Windows\Microsoft.NET\Framework\v3.5"
-    $env:PATH+=";C:\Windows\Microsoft.NET\Framework64\v2.0.50727"
-    $env:PATH+=";C:\Windows\Microsoft.NET\Framework\v2.0.50727"
-    $env:PATH+=";C:\Program Files\Microsoft SDKs\Windows\v7.0\Setup"
-    $env:PATH+=";C:\Program Files\Microsoft SDKs\Windows\v7.0\Bin"    
+    $NewPATH+=";C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE"
+    $NewPATH+=";C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages"
+    $NewPATH+=";C:\Windows\Microsoft.NET\Framework64\v3.5;"
+    $NewPATH+=";C:\Windows\Microsoft.NET\Framework\v3.5"
+    $NewPATH+=";C:\Windows\Microsoft.NET\Framework64\v2.0.50727"
+    $NewPATH+=";C:\Windows\Microsoft.NET\Framework\v2.0.50727"
+    $NewPATH+=";C:\Program Files\Microsoft SDKs\Windows\v7.0\Setup"
+    $NewPATH+=";C:\Program Files\Microsoft SDKs\Windows\v7.0\Bin"
+
+    if ($cpu -eq "x86" -or $cpu -eq "amd64") {
+        if ($env:SETCOMPILER_LIB) {
+            $env:LIB = $env:LIB.replace($env:SETCOMPILER_LIB, $NewLIB)
+        } else {
+            $env:LIB += ";" + $NewLIB
+        }
+        $env:SETCOMPILER_LIB = $NewLIB
+
+        if ($env:SETCOMPILER_INCLUDE) {
+            $env:INCLUDE = $env:INCLUDE.replace($env:SETCOMPILER_INCLUDE, $NewINCLUDE)
+        } else {
+            $env:INCLUDE += ";" + $NewINCLUDE
+        }
+        $env:SETCOMPILER_INCLUDE = $NewINCLUDE
+
+        if ($env:SETCOMPILER_PATH) {
+            $env:PATH = $env:PATH.replace($env:SETCOMPILER_PATH, $NewPATH)
+        } else {
+            $env:PATH += ";" + $NewPATH
+        }
+
+        $env:SETCOMPILER_PATH = $NewPATH
+    }
     
+    
+
     $env:CL = "/MP"
     $env:MSSdk="C:\Program Files\Microsoft SDKs\Windows\v7.0"
     
