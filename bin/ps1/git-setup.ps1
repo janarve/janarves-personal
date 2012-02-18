2803a7c4fc1350990d4840f920d67072294a291f
Jan-Arve Saether <jan-arve.saether@nokia.com>
2012-02-19 00:14:02 +0100

    Setup commit-msg

--- bin/ps1/git-setup.ps1
+++ bin/ps1/git-setup.ps1
@@ -1,7 +1,9 @@
 function GitConfigSet($var, $value)
 {
-	git config --global --unset-all $var
-	git config --global --add $var $value
+    $oldValue = `git config --global --get $var`
+    if ($value -ne $oldValue) {
+        git config --global --add $var $value
+    }
 	return
 }
 
@@ -10,7 +12,7 @@ function Get-LocalOrParentPath($path) {
     while ($checkIn -ne $NULL) {
         $pathToTest = [System.IO.Path]::Combine($checkIn.fullname, $path)
         if (Test-Path $pathToTest) {
-            return $pathToTest
+            return $checkIn.fullname;
         } else {
             $checkIn = $checkIn.parent
         }
@@ -18,10 +20,6 @@ function Get-LocalOrParentPath($path) {
     return $null
 }
 
-
-Push-Location .
-$drive = Get-Location | Split-Path -Qualifier
-$drive = $drive + "\"
 $loc = Get-LocalOrParentPath ".git"
 
 if (!$loc) {
@@ -30,8 +28,25 @@ if (!$loc) {
 }
 Write-Host "Found git repository at $loc"
 
-if (!(Test-Path "$curr\.git\hooks\post-commit")) {
+if (!(Test-Path "$loc\.git\hooks\post-commit")) {
 	Write-Host "Configuring git post-commit hook (not implemented)"
+}
+
+$copyCommitMsg = $false
+if ((Test-Path "$loc\.git\hooks\commit-msg")) {
+    $commitMsgSize = (Get-ItemProperty .git\hooks\commit-msgd).Length
+    if ($commitMsgSize -lt 200) {
+        $copyCommitMsg = $true
+    }
+} else {
+    $copyCommitMsg = $true
+}
+
+if ($copyCommitMsg) {
+	Write-Host "Configuring git commit-msg hook"
+    Push-Location "$loc\.git\hooks\"
+    scp -p smd@codereview.qt-project.org:hooks/commit-msg .
+    Pop-Location
 }
 
 Write-Host "Configuring global .gitconfig"
@@ -39,16 +54,16 @@ Write-Host "Configuring global .gitconfig"
 if ("$env:COMPUTERNAME" -eq "PILSEN") {
 	GitConfigSet "url.qtsoftware:.insteadOf" "git@scm.dev.troll.no:"
 }
-git config --global mailmap.file t:\dev\devtools\aliases\mailmap
-git config --global alias.loginternal "log --pretty=tformat:'commit %H%nAuthor: %an <%ae>%n (int): %aN <%aE>%nDate: %%ad%%n%%n%%s%%n%%n%%b'"
 
-GitConfigSet "core.autocrlf" "true";
-GitConfigSet "alias.br" "branch";
-GitConfigSet "alias.st" "status";
-GitConfigSet "alias.co" "checkout";
-GitConfigSet "alias.ce" "config --global -e";
+GitConfigSet "mailmap.file" "t:\dev\devtools\aliases\mailmap"
+GitConfigSet "alias.loginternal" "log --pretty=tformat:'commit %H%nAuthor: %an <%ae>%n (int): %aN <%aE>%nDate: %%ad%%n%%n%%s%%n%%n%%b'"
+GitConfigSet "core.autocrlf" "true"
+GitConfigSet "alias.br" "branch"
+GitConfigSet "alias.st" "status"
+GitConfigSet "alias.co" "checkout"
+GitConfigSet "alias.ce" "config --global -e"
+GitConfigSet "alias.pushgerrit" "push gerrit HEAD:refs/for/master"
 
 # Show resulting configuration
 git config --global --list
 
-Pop-Location
