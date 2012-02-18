73c29e2db51fa5ca5006bfc0acc9c78456d0d8a7
Jan-Arve Saether <jan-arve.saether@nokia.com>
2012-02-19 00:40:24 +0100

    Fix some stupid bugs

--- bin/ps1/git-setup.ps1
+++ bin/ps1/git-setup.ps1
@@ -2,17 +2,20 @@ function GitConfigSet($var, $value)
 {
     $oldValue = `git config --global --get $var`
     if ($value -ne $oldValue) {
+        if ($oldValue) {
+            git config --global --unset-all $var
+        }
         git config --global --add $var $value
     }
-	return
 }
 
+### Borrowed from git-posh project
 function Get-LocalOrParentPath($path) {
     $checkIn = Get-Item .
     while ($checkIn -ne $NULL) {
         $pathToTest = [System.IO.Path]::Combine($checkIn.fullname, $path)
         if (Test-Path $pathToTest) {
-            return $checkIn.fullname;
+            return $pathToTest;
         } else {
             $checkIn = $checkIn.parent
         }
@@ -28,13 +31,30 @@ if (!$loc) {
 }
 Write-Host "Found git repository at $loc"
 
-if (!(Test-Path "$loc\.git\hooks\post-commit")) {
-	Write-Host "Configuring git post-commit hook (not implemented)"
-}
+#if (!(Test-Path "$loc\hooks\post-commit")) {
+    Write-Host "Creating git post-commit hook"
+    $qtRepoToolsLoc = Get-LocalOrParentPath("qtrepotools")
+    if (!$qtRepoToolsLoc) {
+        Write-Host "Could not find qtrepotools\git-hooks\git_post_commit_hook"
+    } else {
+        # mingw-ize the path to the hook so that bash can find it
+        $drive = $qtRepoToolsLoc[0]
+        $hooksDir = "/$drive/" + $qtRepoToolsLoc.substring(3) + "/git-hooks"
+        $hooksDir = $hooksDir.replace("\", "/")
+        $hooksDir = $hooksDir.replace("\", "/")
+        $content =
+@"
+#!/bin/sh
+export PATH=`$PATH:$hooksDir
+exec $hooksDir/git_post_commit_hook
+"@
+        Set-Content "$loc\hooks\post-commit" $content
+    }
+#}
 
 $copyCommitMsg = $false
-if ((Test-Path "$loc\.git\hooks\commit-msg")) {
-    $commitMsgSize = (Get-ItemProperty .git\hooks\commit-msgd).Length
+if ((Test-Path "$loc\hooks\commit-msg")) {
+    $commitMsgSize = (Get-ItemProperty "$loc\hooks\commit-msg").Length
     if ($commitMsgSize -lt 200) {
         $copyCommitMsg = $true
     }
@@ -43,8 +63,8 @@ if ((Test-Path "$loc\.git\hooks\commit-msg")) {
 }
 
 if ($copyCommitMsg) {
-	Write-Host "Configuring git commit-msg hook"
-    Push-Location "$loc\.git\hooks\"
+    Write-Host "Copying git commit-msg hook"
+    Push-Location "$loc\hooks\"
     scp -p smd@codereview.qt-project.org:hooks/commit-msg .
     Pop-Location
 }
@@ -56,7 +76,7 @@ if ("$env:COMPUTERNAME" -eq "PILSEN") {
 }
 
 GitConfigSet "mailmap.file" "t:\dev\devtools\aliases\mailmap"
-GitConfigSet "alias.loginternal" "log --pretty=tformat:'commit %H%nAuthor: %an <%ae>%n (int): %aN <%aE>%nDate: %%ad%%n%%n%%s%%n%%n%%b'"
+GitConfigSet "alias.loginternal" "log --pretty=tformat:'commit %H%nAuthor: %an <%ae>%n (int): %aN <%aE>%nDate: %ad%n%n%s%n%n%b'"
 GitConfigSet "core.autocrlf" "true"
 GitConfigSet "alias.br" "branch"
 GitConfigSet "alias.st" "status"
