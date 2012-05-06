79d42087f9e39e8185d40572b8703317a634142e
Jan-Arve Saether <jan-arve.saether@nokia.com>
2012-05-07 00:51:50 +0200

    Move stuff from $PROFILE to shell.ps1

--- bin/ps1/shell.ps1
+++ bin/ps1/shell.ps1
@@ -11,7 +11,7 @@ function e($path)
 ### Detect Active State Perl
 if (Test-Path "c:\Perl64\bin") {
     $env:Path = "c:\Perl64\bin;" + $env:PATH
-}elseif (Test-Path "c:\Perl\bin") {
+} elseif (Test-Path "c:\Perl\bin") {
     $env:Path = "c:\Perl\bin;" + $env:PATH
 } else {
     Write-Host "ActivePerl not found"
@@ -20,6 +20,8 @@ if (Test-Path "c:\Perl64\bin") {
 ### Detect python
 if (Test-Path "c:\Python26") {
     $env:Path = "c:\Python26;" + $env:Path
+} elseif (Test-Path "c:\Python27") {
+    $env:Path = "c:\Python27;c:\Python27\Scripts;" + $env:Path
 } else {
     Write-Host "Python not found"
 }
@@ -58,8 +60,21 @@ foreach ($libPath in $libCandidates) {
 }
 
 
+
 $env:Path+=";t:\dev\personal\bin\ps1;t:\bin"
 $env:Path+=";c:\Program Files (x86)\Git\bin"
 
+# eeew... Get rid of symbian toolchain (might confuse mingw)
+$env:Path = $env:Path.replace("C:\Program Files (x86)\Common Files\Symbian\tools;", "")
+
+setcompiler x86 msvc2010expr
+$env:CL = "/MP"
+
+# Load posh-git example profile
+. 'T:\dev\posh-git\profile.example.ps1'
+
+. 'qttabexp.ps1'
+
 $env:ARTISTIC_STYLE_OPTIONS="$USERPROFILE\astylerc"
 $env:QT_MESSAGE_PATTERN="%{file}(%{line}):%{message}"
+$env:GIT_TEMPLATE_DIR="t:\dev\devtools\git\template"
