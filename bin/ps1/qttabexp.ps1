e965485c0c919da4087f37eec3de4704a3bb4e89
Jan Arve Saether <jan-arve.saether@digia.com>
2013-02-25 00:09:41 +0100

    Try to consolidate most Qt stuff into one single file

--- bin/ps1/qttabexp.ps1
+++ bin/ps1/qttabexp.ps1
@@ -1,40 +1,5 @@
 setqt 5-x86
 
-if (Test-Path Function:\TabExpansion) {
-
-    $qtteBackup = 'qcd_DefaultTabExpansion'
-    if (!(Test-Path Function:\$qtteBackup)) {
-        Rename-Item Function:\TabExpansion $qtteBackup
-    }
-    Write-Host "installed qcd tab expansion"
-
-    function TabExpansion($line, $lastWord) {
-        $qtdir = Get-QtBasePath
-        if ($qtdir) {
-            $lastBlock = [regex]::Split($line, '[|;]')[-1].TrimStart()
-
-            switch -regex ($lastBlock) {
-                # Execute qcd tab completion
-                "qcd .*" {
-                    $arg = $lastWord
-                    $lastSlash = $arg.lastIndexOf('\')
-                    $lastSlash = $lastSlash + 1
-                    $base = $arg.substring(0, $lastSlash)
-                    foreach ($s in @("src", "examples", ".", "..")) {   # Order must be the same as in qcd
-                        Get-ChildItem "$qtdir\$s\$lastWord*" | ForEach-Object { $base + $_.Name }
-                    }
-                }
-                # Fall back on existing tab expansion
-                default { & $qtteBackup $line $lastWord }
-            }
-        } else {
-            & $qtteBackup $line $lastWord
-        }
-    }
-}
-
-
-
 function detectInstalledLibraries($arch, [ref]$newIncludes, [ref]$newLibs, [ref]$newPaths)
 {
     # "include", "lib", "PATH"
@@ -276,12 +241,190 @@ function Set-Compiler($compilerSpec = "msvc2010", $arch = "x86")
     }
 }
 
+function resolveQtPath($loc)
+{
+   if (Test-Path $loc) {
+        # Try relative and absolute
+        $loc = Resolve-Path $loc
+        if (Test-Path ("$loc\qtbase\bin")) {
+            $loc = "$loc\qtbase"
+        }
+        return $loc
+    }
+    return $null
+}
+
+### Appends $newValue to the environment variable specified by $envName
+### Stores what was appended to $envNameAppendedPortion in order to know
+### how to restore it back to its original form.
+function updateEnvironmentPathValue($envName, $envNameAppendedPortion, $newValue)
+{
+    $result = $null
+
+    $oldVar = $null
+    if (Test-Path -path env:$envNameAppendedPortion) {
+        $oldVar = (Get-Item -path env:$envNameAppendedPortion).Value
+    }
+
+    $nameVar = $null
+    if (Test-Path -path env:$envName) {
+        $nameVar = (Get-Item -path env:$envName).Value
+    }
+
+    if ($oldVar) {
+        $result = $nameVar.replace($oldVar, $newValue)
+    } else {
+        if ($newValue) {
+            if ($nameVar) {
+                $result = $nameVar + ";"
+            }
+            $result += $newValue
+        }
+    }
+    if ($result) {
+        Set-Item -path env:$envName -value "$result"
+        Set-Item -path env:$envNameAppendedPortion -value "$newValue"
+    }
+}
+
+
+function Set-QtPath() {
+<#
+.SYNOPSIS
+A tool to switch between different installed Qt versions
+
+.DESCRIPTION
+The tool will look for Qt on the system using various heuristics. The path can
+be either an absolute or relative path. If it is relative it is either relative
+to the current working directory or to the path specified by the QTREPOS
+environment variable.
+Paths matching these criterias must either have a <qtdir>\bin folder (for Qt 4
+series) or a <qtdir>\qtbase\bin folder.
+
+It tries to find the desired Qt version by 3 different strategies,
+in the following order:
+1. If an absolute path is given and it exist, the path will be used.
+2. If an relative path is given and it exist, the corresponding absolute
+   path of the relative path will be used.
+3. If QTREPOS is set it will check if there exists a directory located
+   under $QTREPOS named qt-$qtdir. If that exist, it will set QTDIR
+   to $QTREPOS\qt-$qtdir. (Recommended usage)
+
+.EXAMPLE
+Set-QtPath c:\projects\qt-5\qtbase
+Will switch to the Qt version installed at c:\projects\qt-5\qtbase, if it exists.
+
+.EXAMPLE
+Set-QtPath qt-48
+If current directory is c:\projects, will set QTDIR to c:\projects\qt-48, if it exists.
+
+.EXAMPLE
+Set-QtPath none
+Will unset QTDIR environment variable and remove $env:QTDIR\bin from the PATH environment variable.
+
+.EXAMPLE
+Set-QtPath .
+Will set QTDIR to the current directory.
+
+.EXAMPLE
+Set-QtPath 5\qtbase
+If QTREPOS is set to c:\dev, it will switch to the Qt version installed
+at c:\dev\qt-5\qtbase
+
+.EXAMPLE
+Set-QtPath 5
+If QTREPOS is set to c:\dev, it will switch to the Qt version installed
+at c:\dev\qt-5\qtbase
+
+.EXAMPLE
+Set-QtPath 47
+If QTREPOS is set to c:\dev, it will switch to the Qt version installed
+at c:\dev\qt-47
+
+.LINK
+http://jans.tihlde.org
+#>
+param([string]$qtdir = $null, [switch]$clean)
+
+    if ($clean){
+        Write-Host "Cleaning up QTREPOS environment variable"
+        [Environment]::SetEnvironmentVariable("QTREPOS", "", "User")
+        $Env:QTREPOS = ""
+    } elseif ($qtdir) {
+        if (!($qtdir -eq "none")) {
+            $newQTDIR = resolveQtPath($qtdir)
+            if (!$newQTDIR) {
+                if (!$Env:QTREPOS) {
+                    $qtRepos = Read-host "Please enter the complete path to your Qt development area (T:\dev)"
+                    if (!$qtRepos) {
+                        $qtRepos = "T:\dev"
+                    }
+                    $Env:QTREPOS = $qtRepos
+                    $storeQtdep = Read-host "Do you want to set the location of your Qt depot as a permanent setting?(y/n)"
+                    if ($storeQtdep -eq "y") {
+                        Write-Host "Qt depot location stored as permanent environment variable. To remove, run .\setqt.ps1 -clean"
+                        [Environment]::SetEnvironmentVariable("QTREPOS", "$qtRepos", "User")
+                    } else {
+                        if (!($storeQtdep -eq "n")) {
+                            Write-Host "Invalid input"
+                        }
+                        Write-Host "Qt depot location temporarily stored and will be available in this session only."
+                    }
+                }
+                $newQTDIR = resolveQtPath($Env:QTREPOS + "\qt-" + $qtdir)
+            }
+        }
+        if ($newQTDIR -or $qtdir -eq "none") {
+            # If successful, first remove old QTDIR\bin from PATH, then unset $env:QTDIR
+            if ($env:SETQT_PATH) {
+                $pattern = $env:SETQT_PATH
+                $env:PATH = $env:PATH.replace(";$pattern", "")      #doing it twice in case its the first or last item in the Path
+                $env:PATH = $env:PATH.replace("$pattern;", "")
+
+                if ($setQTDIR) {
+                    $env:QTDIR = $null
+                }
+                $env:SETQT_PATH = $null
+            }
+
+            if ($newQTDIR) {
+                if ($setQTDIR) {
+                    $Env:QTDIR = $newQTDIR
+                }
+                updateEnvironmentPathValue "PATH" "SETQT_PATH" "$newQTDIR\bin"
+                Write-Host "Qt version is set to $newQTDIR"
+            } else {
+                # ($qtdir -eq "none")
+                Write-Host "Qt version is unset."
+            }
+        } else {
+            Write-Host "Could not find the given Qt version"
+        }
+    } else {
+        if ($Env:QTDIR){
+            Write-Host "$Env:QTDIR"
+        } elseif ($Env:SETQT_PATH) {
+            Write-Host ($Env:SETQT_PATH -replace "\\bin", "")
+        } else {
+            Write-Host "QTDIR is not set"
+        }
+    }
+}
+
 function Get-QtBasePath() {
     if ($env:SETQT_PATH) {
         (Resolve-Path "$env:SETQT_PATH\..").Path
     } else {
-#    (& qmake -query QT_INSTALL_PREFIX) -replace "/", "\"
-        return $null
+        # Try by looking in PATH environment
+        $res = Get-Command syncqt.bat
+        if ($res) {
+            if ($res.GetType() -eq [Object[]]) {
+                $res = res[0]
+            }
+            $res = $res.Definition
+            $qtdir = Resolve-Path "$res\..\.."
+            return $qtdir.Path
+        }
     }
 }
 
@@ -290,4 +433,95 @@ function Generate-Pro-File()
     & qmake -project
 }
 
+function Set-QtLocation() {
+<#
+.SYNOPSIS
+Changes working directory to a folder within the current Qt version.
+
+.DESCRIPTION
+This tool is used together with Set-QtPath, and it will change the current
+working directory to the last Qt version that Set-QtPath changed to.
+If an argument is supplied, it will look in subfolders of the current Qt
+version.
+
+.EXAMPLE
+Suppose the following precondition:
+Set-QtPath c:\projects\qt-5\qtbase
+cd c:\temp
+Set-QtLocation
+will change current working directory back to c:\projects\qt-5\qtbase
+
+#>
+param([string]$subdir)
+
+    $qtdir = Get-QtBasePath
+
+    if (!$qtdir) {
+        Write-Host "Could not detect location of Qt, neither through QTDIR or SETQT_PATH. Run setqt <version>"
+        return
+    }
+
+    if (!$subdir) {
+        Push-Location $qtdir
+    } else {
+        foreach ($s in @("src", "examples", ".", "..")) {
+            $dest = "$qtdir\$s\$subdir"
+            if (Test-Path $dest) {
+                Push-Location $dest
+                return
+            }
+        }
+        Write-Host "$subdir not found in QTDIR, .., src or examples"
+        return
+    }
+}
+
+if (Test-Path Function:\TabExpansion) {
+
+    $qtteBackup = 'qcd_DefaultTabExpansion'
+    if (!(Test-Path Function:\$qtteBackup)) {
+        Rename-Item Function:\TabExpansion $qtteBackup
+    }
+    Write-Host "installed qcd tab expansion"
+
+    function TabExpansion($line, $lastWord) {
+        $qtdir = Get-QtBasePath
+        if ($qtdir) {
+            $lastBlock = [regex]::Split($line, '[|;]')[-1].TrimStart()
+            $command = ($lastBlock -split " ")[0]
+            # unwrap aliases
+            $realCommand = (Get-Alias $command).Definition
+            if (!$realCommand) {
+                $realCommand = $command
+            }
+            switch ($realCommand) {
+                # Execute qcd tab completion
+                "Set-QtLocation" {
+                    $arg = $lastWord
+                    $lastSlash = $arg.lastIndexOf('\')
+                    $lastSlash = $lastSlash + 1
+                    $base = $arg.substring(0, $lastSlash)
+                    foreach ($s in @("src", "examples", ".", "..")) {   # Order must be the same as in qcd
+                        Get-ChildItem "$qtdir\$s\$lastWord*" | ForEach-Object { $base + $_.Name }
+                    }
+                }
+                "Set-QtPath" {
+                    $arg = $lastWord
+                    $qtRepos = $env:QTREPOS
+                    if ($qtRepos) {
+                        Get-ChildItem "$qtRepos/qt-$arg*" | ForEach-Object { $_.Name -replace "qt-"}
+                    }
+                }
+                # Fall back on existing tab expansion
+                default { & $qtteBackup $line $lastWord }
+            }
+        } else {
+            & $qtteBackup $line $lastWord
+        }
+    }
+}
+
+
 Set-Alias qp "Generate-Pro-File"
+Set-Alias setqt "Set-QtPath"
+Set-Alias qcd "Set-QtLocation"
